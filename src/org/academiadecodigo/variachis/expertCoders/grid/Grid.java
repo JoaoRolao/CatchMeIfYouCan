ba630c6d337a93ad1d9761154ce5c8c2577e095b
Tiago Machado <tfamsoad@gmail.com>
2018-10-04 23:39:14 +0100

    updated grid

--- src/org/academiadecodigo/variachis/expertCoders/grid/Grid.java
+++ src/org/academiadecodigo/variachis/expertCoders/grid/Grid.java
@@ -2,12 +2,37 @@ package org.academiadecodigo.variachis.expertCoders.grid;
 
 import org.academiadecodigo.variachis.expertCoders.interfaces.Collidable;
 
-public class Grid implements Collidable {
+public class Grid implements Collidable { //implements the -->Collidable interface (-->Drawable)
+
+    //properties of the grid
+    private int rows;
+    private int cols;
+    private int width;
+    private int heigth;
+
+    public Grid(int rows, int cols){
+
+        this.rows = rows;
+        this.cols = cols;
+
+
+    }
 
 
 
+    //GETTER FOR THE POSITION CLASS TO USE --->COLUMNS
+    public int getCols() {
+        return cols;
+    }
+
+    //GETTER FOR THE POSITION CLASS TO USE --->ROWS
+    public int getRows() {
+        return rows;
+    }
 
 
+
+    //*************inherited interface methods ---TO WORK--- *********************
     @Override
     public boolean checkColision() {
         return false;
