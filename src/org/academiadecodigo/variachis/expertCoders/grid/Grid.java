8bbd7c174a5bca27477d2831e7e2280e2516c0df
Tiago Machado <tfamsoad@gmail.com>
2018-10-05 16:44:03 +0100

    updated grid

--- src/org/academiadecodigo/variachis/expertCoders/grid/Grid.java
+++ src/org/academiadecodigo/variachis/expertCoders/grid/Grid.java
@@ -39,13 +39,9 @@ public class Grid implements Collidable { //implements the -->Collidable interfa
 
     }
     */
-
-<<<<<<< HEAD
-
     //*************inherited interface methods ---TO WORK--- *********************
     @Override
-=======
->>>>>>> dd5f7ceda39a1e623e667a25e0a8e2ad94e109ce
+
     public void draw() {
         System.out.println("im a grid (draw method");
     }
@@ -55,9 +51,5 @@ public class Grid implements Collidable { //implements the -->Collidable interfa
     public boolean checkCollision() {
         return false;
     }
-<<<<<<< HEAD
-=======
 
-
->>>>>>> dd5f7ceda39a1e623e667a25e0a8e2ad94e109ce
 }
--- src/org/academiadecodigo/variachis/expertCoders/player/Player.java
+++ src/org/academiadecodigo/variachis/expertCoders/player/Player.java
@@ -15,11 +15,7 @@ public class Player extends AbstractCollidable {
 
     public Player(){
 
-<<<<<<< HEAD
-
-
     }
 
-=======
->>>>>>> 840c7b50349b9b465e6ba862f6e6094060d217f1
+
 }
