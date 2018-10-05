1e8f0c2feb6875b8d990abfef28df0736f8ac803
Tiago Machado <tfamsoad@gmail.com>
2018-10-05 14:57:33 +0100

    updated grid

--- src/org/academiadecodigo/variachis/expertCoders/grid/Grid.java
+++ src/org/academiadecodigo/variachis/expertCoders/grid/Grid.java
@@ -7,8 +7,6 @@ public class Grid implements Collidable { //implements the -->Collidable interfa
     //properties of the grid
     private int rows;   //grid has rows
     private int cols;   //grid has columns
-    private int width = 800;  //grid has width(largura) of 800
-    private int height = 800; //grid has height(altura) of 800
 
     //constructor -> initializing the properties -> delegating to Position since Position HAS-A grid
     public Grid(int rows, int cols) {
@@ -29,16 +27,6 @@ public class Grid implements Collidable { //implements the -->Collidable interfa
         return rows;
     }
 
-
-    //GETTER FOR THE POSITION CLASS TO USE --->HEIGHT(ALTURA) ---> POSITION KNOWS THE HEIGHT AND WIDTH TO HAVE A POSITION (????)
-    public int getHeight() {
-        return height;
-    }
-
-    //GETTER FOR THE POSITION CLASS TO USE --->WIDTH(LARGURA) ---> POSITION KNOWS THE WIDTH AND HEIGHT TO HAVE A POSITION (????)
-    public int getWidth() {
-        return width;
-    }
 
     //*************inherited interface methods ---TO WORK--- *********************
 
