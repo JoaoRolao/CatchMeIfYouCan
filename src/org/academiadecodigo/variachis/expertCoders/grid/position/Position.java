7d0b29b379c3a454474e75afe317b9f091ff9899
daf78 <david.afonso.fr@gmail.com>
2018-10-05 16:56:34 +0100

    added modifications to PositionFactory and Position classes

--- src/org/academiadecodigo/variachis/expertCoders/grid/position/Position.java
+++ src/org/academiadecodigo/variachis/expertCoders/grid/position/Position.java
@@ -1,4 +1,31 @@
 package org.academiadecodigo.variachis.expertCoders.grid.position;
 
 public class Position {
+
+    private int col;
+    private int row;
+    private Grid grid;
+    private boolean collided = false;
+
+    public Position(int col, int row, Grid grid) {
+
+        this.col = col;
+        this.row = row;
+        this.grid = grid;
+    }
+
+    public void move() {
+
+
+    }
+
+    public void draw() {
+
+
+    }
+
+    public void checkCollision() {
+
+
+    }
 }
--- src/org/academiadecodigo/variachis/expertCoders/grid/position/PositionFactory.java
+++ src/org/academiadecodigo/variachis/expertCoders/grid/position/PositionFactory.java
@@ -1,4 +1,14 @@
 package org.academiadecodigo.variachis.expertCoders.grid.position;
 
+/**
+ * A factory to create different positions for different itens.
+ */
+
 public class PositionFactory {
+
+    public static Position getNewPosition (Grid grid) {
+
+
+    }
+
 }
